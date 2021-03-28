e91342dfc05be93308ff9f9ae19434bbc8e2ed8d
leiqlxk <472990991@qq.com>
2021-03-28 22:59:36 +0800

    初始化项目

---
+++ src/test/java/Tests.java
@@ -0,0 +1,11 @@
+/**
+ * Title: test <br>
+ * ProjectName: learn-data-structure <br>
+ * description: TODO <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/3/28 22:58 <br>
+ */
+public class Tests {
+}
