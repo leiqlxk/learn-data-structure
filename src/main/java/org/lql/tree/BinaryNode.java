7ce938b88f1bae20832caa90f9bda06a72d693ec
leiqlxk <472990991@qq.com>
2021-03-30 16:44:10 +0800

    二叉树

---
+++ src/main/java/org/lql/tree/BinaryNode.java
@@ -0,0 +1,17 @@
+package org.lql.tree;
+
+/**
+ * Title: BinaryNode <br>
+ * ProjectName: learn-data-structure <br>
+ * description: 二叉树节点 <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/3/30 15:52 <br>
+ */
+public class BinaryNode {
+
+    Object element;
+    BinaryNode left;
+    BinaryNode right;
+}
