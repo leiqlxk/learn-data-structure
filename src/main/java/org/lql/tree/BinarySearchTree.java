e7f8f1eb8249b63eb72b7fa514fe1b23ac3f6033
leiqlxk <472990991@qq.com>
2021-03-30 22:36:20 +0800

    二叉查找树

--- src/main/java/org/lql/tree/BinarySearchTree.java
+++ src/main/java/org/lql/tree/BinarySearchTree.java
@@ -38,36 +38,116 @@ public class BinarySearchTree<E extends Comparable<? super E>> {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
         }
-        
+
         return this.findMin(this.root).element;
     }
-    
+
     public E findMax() {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
         }
-        
+
         return this.findMax(this.root).element;
     }
 
     public void insert(E e) {
         root = this.insert(e, this.root);
     }
-    
+
     public void remove(E e) {
         root = this.remove(e, this.root);
     }
 
-    private BinaryNode<E> remove(E e, BinaryNode<E> root) {
+    /**
+     * description: 删除时如果节点是叶子节点可以直接删除，如果节点有一个子节点，
+     * 则该节点可以在其父节点调整自己的链接以绕过该节点，如果节点有两个子节点，
+     * 一般的删除策略是用其右子树的最小数据代替该节点的数据并递归地删除那个节点。<br>
+     *
+     * @author: leiql <br>
+     * @version: 1.0 <br>
+     * @since: 2021/3/30 21:49 <br>
+     *
+     * @throws
+     * @param e
+     * @param t
+     * @return org.lql.tree.BinarySearchTree.BinaryNode<E>
+     */
+    private BinaryNode<E> remove(E e, BinaryNode<E> t) {
+        if (t == null) {
+            return t;
+        }
+
+        int compareResult = e.compareTo(t.element);
+
+        if (compareResult < 0) {
+            t.left = this.remove(e, t.left);
+        }else if (compareResult > 0) {
+            t.right = this.remove(e, t.right);
+        }else  if (t.left != null && t.right !=  null) {
+            t.element = this.findMin(t.right).element;
+            t.right = this.remove(t.element, t.right);
+        }else {
+            t = (t.left != null) ? t.left : t.right;
+        }
+
+        return t;
     }
 
-    private BinaryNode<E> insert(E e, BinaryNode<E> root) {
+
+    private BinaryNode<E> insert(E e, BinaryNode<E> t) {
+        if (t == null) {
+            return new BinaryNode<>(e, null, null);
+        }
+
+        int compareResult = e.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = this.insert(e, t.left);
+        }else if (compareResult > 0) {
+            t.right = this.insert(e, t.right);
+        }
+
+        return t;
     }
 
-    private BinaryNode<E> findMax(BinaryNode<E> root) {
+    /**
+     * description: 查找最大值朝右子节点进行 <br>
+     *
+     * @author: leiql <br>
+     * @version: 1.0 <br>
+     * @since: 2021/3/30 21:39 <br>
+     *
+     * @throws
+     * @param t
+     * @return org.lql.tree.BinarySearchTree.BinaryNode<E>
+     */
+    private BinaryNode<E> findMax(BinaryNode<E> t) {
+        if (t != null) {
+            while (t.right != null) {
+                t = t.right;
+            }
+        }
+        return t;
     }
 
-    private BinaryNode<E> findMin(BinaryNode<E> root) {
+    /**
+     * description: 查找最小值从根开始并且只要有左子节点就向左进行，终点就是最小元素 <br>
+     *
+     * @author: leiql <br>
+     * @version: 1.0 <br>
+     * @since: 2021/3/30 21:37 <br>
+     *
+     * @throws
+     * @param t
+     * @return org.lql.tree.BinarySearchTree.BinaryNode<E>
+     */
+    private BinaryNode<E> findMin(BinaryNode<E> t) {
+        if(t == null) {
+            return null;
+        }else if (t.left == null) {
+            return t;
+        }
+
+        return this.findMin(t.left);
     }
 
     /**
@@ -97,11 +177,6 @@ public class BinarySearchTree<E extends Comparable<? super E>> {
         }else {
             return true;
         }
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        return 0;
     }
 
     private static class BinaryNode<E> {
