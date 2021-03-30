ef664d4f2f1a31e0261202539e132d98f46086dc
leiqlxk <472990991@qq.com>
2021-03-30 15:09:51 +0800

    树基础

---
+++ src/main/java/org/lql/tree/TreeNode.java
@@ -0,0 +1,17 @@
+package org.lql.tree;
+
+/**
+ * Title: TreeNode <br>
+ * ProjectName: learn-data-structure <br>
+ * description: 树节点 <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/3/30 14:43 <br>
+ */
+public class TreeNode {
+
+    Object element;
+    TreeNode firstChild;
+    TreeNode nextSibling;
+}
